5905a9a3d861d200374f05e3a512e09a2940283a
Akwarski <akwarski@gmail.com>
2019-08-09 08:36:02 +0200

    update

--- app/src/main/java/akwarski/pcss/adapter/ExpandableListViewAdapter.kt
+++ app/src/main/java/akwarski/pcss/adapter/ExpandableListViewAdapter.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseExpandableListAdapter
-import android.widget.TextView
 import android.widget.Toast
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
